2160d828e35550f49b37c58d7badffca0323ddfe
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-27 05:44:25 +0000

    Implement character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+
+  $("#tweet-text").on("input", function(event) {
+    const charsLeft = 140 - $(this).val().length; //val is getting the text from text area - not setting new value
+
+    let counter = $(this).siblings().children(".counter").text(charsLeft);
+    
+    if (charsLeft < 0) {
+      counter.addClass("add-color");
+    } else {
+      counter.removeClass("add-color");
+    }
+  });
 });
+
+
+
+// $("#tweet-text").keyup(function(){
+//   $(".counter").text((140 - $(this).val().length));
+// });
+
