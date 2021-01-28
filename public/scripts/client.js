e7653123c1570dbc57b8d2309ca6980dfc1f848e
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-28 03:59:01 +0000

    Implement fetching tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,37 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
 
 $(document).ready(function () {
-
-
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
 
   const renderTweets = function (tweets) {
     // loops through tweets
@@ -73,21 +44,27 @@ $(document).ready(function () {
     return $tweet;
   };
 
-  renderTweets(tweetData);
+  const loadTweets = function () {
+
+    $.ajax('/tweets', {
+      dataType: 'json',
+      method: 'GET'
+    })
+      .then((result) => {
+        renderTweets(result);
+      });
+  };
+
+  loadTweets();
 
   $("form").on("submit", function (event) {
     event.preventDefault();
 
     let url = '/tweets/';
 
-    // $.ajax({
-    //   url: url,
-    //   method: "POST",
-    //   data: $(this).serialize();
-    // });
-
     // refactored POST method in AJAX
-    $.post(url, $(this).serialize())
+    $.post(url, $(this).serialize());
 
   });
+
 });
