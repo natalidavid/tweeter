56071d9ebfbfbc5a3494b23cc9a24ed64b49d5f2
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-28 01:53:27 +0000

    Implement dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,42 +6,71 @@
 
 // Fake data taken from initial-tweets.json
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+$(document).ready(function () {
+
+
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
 
 
-const renderTweets = function(tweets) {
-// loops through tweets
-// calls createTweetElement for each tweet
-// takes return value and appends it to the tweets container
-}
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (let tweet of tweets) {
+      const element = createTweetElement(tweet);
+      $('#tweets').prepend(element);
 
-const createTweetElement = function(tweet) {
-let $tweet = /* Your code for creating the tweet element */
-// ...
-return $tweet;
-}
+    }
+  };
 
-renderTweets(data);
+  const createTweetElement = function (tweet) {
+
+    let $tweet = /* Your code for creating the tweet element */
+      // ...
+      `<article class="tweet">
+          <header>
+            <div class="image-username">
+              <img src="${tweet.user.avatars}">
+              <span class="author">${tweet.user.name}</span>
+            </div>
+            <span class="handle">${tweet.user.handle}</span>
+          </header>
+          <p class="tweet-status">${tweet.content.text}</p>
+          <footer>
+            <span>${tweet.created_at}</span>
+            <div class="feed-icons">
+              <i class="fas fa-flag"></i>
+              <i class="fas fa-retweet"></i>
+              <i class="fas fa-heart"></i>
+            </div>
+          </footer>
+        </article>`;
+
+    return $tweet;
+  };
+
+  renderTweets(tweetData);
